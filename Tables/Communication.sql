cb4f87491c327336ddd4d13184792394849e8bd0
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-09-14 15:30:57 +0300

    Create Communication.sql

---
+++ Tables/Communication.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Communication(
+	CommunicationID INT NOT NULL AUTO_INCREMENT,
+    -- This is the Email Social Media account Phone etc...
+    Type VARCHAR(15) NOT NULL,
+    
+    Primary Key(CommunicationID)
+) ENGINE = InnoDB;
